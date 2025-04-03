ac438d08e31e6a654683de9344a63ee6e0aef56c
Guilherme Maioli <guilherme.maioli@dadosfera.io>
2025-04-03 07:40:34 -0300

    Remove function

--- src/redshift.py
+++ src/redshift.py
@@ -41,20 +41,4 @@ class Redshift:
         
         return df
     
-    def load_jsons(self):
-        
-        conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=postgresql port=5435")
-        cursor = conn.cursor()
-        
-        path_json = f"/tmp/{self.table}"
-        for name_file in os.listdir(path_json):
-            if not name_file.endswith(".json"): 
-                continue
-
-            with open(f'/tmp/{self.table}/{name_file}', "r") as f:
-                #json_data = f.read()
-
-                with conn.cursor() as cursor:
-                    cursor.copy_expert("COPY person_identifier(identifier, identifier_value, person_id) FROM STDIN", f)
-                    conn.commit()
 
